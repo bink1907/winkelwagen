6310ae67c29125f0d5cc251947c3cce885ecf4dc
Bink de Kievit <binkdekievit@Binks-MacBook-Pro.local>
2019-09-03 17:53:48 +0200

    Initial Commit

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategorieRepository")
+ */
+class Categorie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $naam;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="categorie_id")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNaam(): ?string
+    {
+        return $this->naam;
+    }
+
+    public function setNaam(string $naam): self
+    {
+        $this->naam = $naam;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setCategorieId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->contains($product)) {
+            $this->products->removeElement($product);
+            // set the owning side to null (unless already changed)
+            if ($product->getCategorieId() === $this) {
+                $product->setCategorieId(null);
+            }
+        }
+
+        return $this;
+    }
+    public function __toString()
+    { return strval($this->id); }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $naam;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prijs;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $omschrijving;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $afbeelding;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Categorie", inversedBy="products")
+     */
+    private $categorie_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNaam(): ?string
+    {
+        return $this->naam;
+    }
+
+    public function setNaam(?string $naam): self
+    {
+        $this->naam = $naam;
+
+        return $this;
+    }
+
+    public function getPrijs(): ?float
+    {
+        return $this->prijs;
+    }
+
+    public function setPrijs(float $prijs): self
+    {
+        $this->prijs = $prijs;
+
+        return $this;
+    }
+
+    public function getOmschrijving(): ?string
+    {
+        return $this->omschrijving;
+    }
+
+    public function setOmschrijving(?string $omschrijving): self
+    {
+        $this->omschrijving = $omschrijving;
+
+        return $this;
+    }
+
+    public function getAfbeelding(): ?string
+    {
+        return $this->afbeelding;
+    }
+
+    public function setAfbeelding(string $afbeelding): self
+    {
+        $this->afbeelding = $afbeelding;
+
+        return $this;
+    }
+
+    public function getCategorieId(): ?Categorie
+    {
+        return $this->categorie_id;
+    }
+
+    public function setCategorieId(?Categorie $categorie_id): self
+    {
+        $this->categorie_id = $categorie_id;
+
+        return $this;
+    }
+
+}
